9348486960be5c928a2f1854cee313bc13d34b94
Matthew Dobson <mdobson4@gmail.com>
2015-12-22 09:03:44 -0500

    Updates to admin app. Twitch authentication functionality.

---
+++ ui/auth.js
@@ -0,0 +1,16 @@
+//Authenticate with twitch
+Twitch.init({clientId: creds.clientId}, function(error, status) {
+  if(status.authenticated) {
+    $('.twitch-connect').hide(); 
+    var token = Twitch.getToken();
+    $('#token').html('Your token is: ' + token);
+  } else {
+    $('.twitch-connect').on('click', function() {
+      console.log('clicked');
+      Twitch.login({
+        scope: ['user_read', 'channel_read']
+      });  
+    });   
+  }
+});
+
