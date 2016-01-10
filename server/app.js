6141a681826d07daa05c89fb495b82be43bf65e9
Matthew Dobson <mdobson4@gmail.com>
2016-01-10 09:55:15 -0500

    Playing with the messages stream.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 module.exports = function(server) {
   var twitchQuery = server.where({ type: 'twitch-chat-bot' });
   server.observe([twitchQuery], function(bot) {
+    console.log('found bot');
     var botMessages = bot.createReadStream('messages');
     
     botMessages.on('data', function(message) {
+      console.log(message);
     });  
-  }):   
+  });   
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var zetta = require('zetta');
 var twitch = require('./scout');
+var app = require('./app');
 
 zetta()
   .use(twitch)
+  .use(app)
   .listen(1337);
